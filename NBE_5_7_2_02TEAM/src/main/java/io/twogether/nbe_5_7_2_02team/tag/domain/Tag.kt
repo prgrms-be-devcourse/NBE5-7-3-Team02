7e33a0acbaeeb327c642038fd25f42ddde502216
Jeongmin39 <wjdals20309@naver.com>
2025-06-16 11:29:28 +0900

    migration: Tag domain 마이그레이션

---
+++ NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/tag/domain/Tag.kt
@@ -0,0 +1,25 @@
+package io.twogether.nbe_5_7_2_02team.tag.domain
+
+import io.twogether.nbe_5_7_2_02team.post.domain.PostTag
+import jakarta.persistence.*
+
+@Entity
+class Tag(
+    @Column(nullable = false)
+    val name: String
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+    @OneToMany(mappedBy = "tag", fetch = FetchType.LAZY)
+    val postTags: MutableList<PostTag> = mutableListOf()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Tag) return false
+        return name == other.name
+    }
+
+    override fun hashCode(): Int = name.hashCode()
+}
