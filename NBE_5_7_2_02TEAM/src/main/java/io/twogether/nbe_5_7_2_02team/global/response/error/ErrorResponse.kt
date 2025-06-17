7130a5bc4477470a34b0aed3bc3a5d545f9097e3
한정민 <80705450+Jeongmin39@users.noreply.github.com>
2025-06-18 00:18:02 +0900

    migration: global 내 exception 및 response/error 패키지 마이그레이션 (#42)
    * migration: ErrorStatus 마이그레이션
    
    * migration: ErrorCode 마이그레이션
    
    * migration: ErrorResponse 마이그레이션
    
    * migration: ErrorException 마이그레이션
    
    * migration: ExceptionAdvice 마이그레이션
    
    * chore: Kotlin 스타일 수정
    
    * empty commit
    
    ---------
    
    Co-authored-by: dlsrks1021 <dlsrks1021@gmail.com>

---
+++ NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/global/response/error/ErrorResponse.kt
@@ -0,0 +1,13 @@
+package io.twogether.nbe_5_7_2_02team.global.response.error
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder("code", "message", "errors")
+data class ErrorResponse<T>(
+    val code: String,
+    val message: String,
+    // 추가적인 오류 세부 정보를 담기 위한 필드
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    val errors: T? = null,
+)
