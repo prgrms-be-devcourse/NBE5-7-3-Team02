0029d90a37394bdcbf09c8df9a4ea32ca7b35616
Jeongmin39 <wjdals20309@naver.com>
2025-06-18 15:44:34 +0900

    migration: WebConfig 마이그레이션

---
+++ NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/global/config/WebConfig.kt
@@ -0,0 +1,24 @@
+package io.twogether.nbe_5_7_2_02team.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.nio.file.Paths
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    companion object {
+        private const val UPLOAD_PATH = "/uploads/**"
+        private const val UPLOAD_DIR = "uploads"
+        private const val CACHE_PERIOD = 3600
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        val uploadLocation = "file:${Paths.get(UPLOAD_DIR).toAbsolutePath()}/"
+
+        registry.addResourceHandler(UPLOAD_PATH)
+            .addResourceLocations(uploadLocation)
+            .setCachePeriod(CACHE_PERIOD)
+    }
+}
