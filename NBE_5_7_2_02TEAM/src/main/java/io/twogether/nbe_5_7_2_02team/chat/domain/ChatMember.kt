540be106dc242ffb10a5ff0b5cd68c50a54a4a55
MyungJin <audwls239@naver.com>
2025-06-18 10:43:33 +0900

    migration: Entity 마이그레이션

--- NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/chat/domain/ChatMember.kt
+++ NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/chat/domain/ChatMember.kt
@@ -1,62 +1,24 @@
-package io.twogether.nbe_5_7_2_02team.chat.domain;
+package io.twogether.nbe_5_7_2_02team.chat.domain
 
-import io.twogether.nbe_5_7_2_02team.global.common.BaseEntity;
-import io.twogether.nbe_5_7_2_02team.member.domain.Member;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import io.twogether.nbe_5_7_2_02team.global.common.BaseEntity
+import io.twogether.nbe_5_7_2_02team.member.domain.Member
+import jakarta.persistence.*
 
 @Entity
-public class ChatMember extends BaseEntity {
-
+data class ChatMember(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    val id: Long? = null,
 
-    @ManyToOne
     @JoinColumn(name = "chatroom_id")
-    private ChatRoom chatRoom;
-
     @ManyToOne
+    val chatRoom: ChatRoom,
+
     @JoinColumn(name = "member_id")
-    private Member member;
+    @ManyToOne
+    val member: Member,
 
-    @Column(name = "status")
     @Enumerated(EnumType.STRING)
-    private ChatMemberStatus chatMemberStatus;
-
-    public ChatMember(ChatRoom chatRoom, Member member, ChatMemberStatus chatMemberStatus) {
-        this.chatRoom = chatRoom;
-        this.member = member;
-        this.chatMemberStatus = chatMemberStatus;
-    }
-
-    protected ChatMember() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public ChatRoom getChatRoom() {
-        return chatRoom;
-    }
-
-    public void setChatMemberStatus(ChatMemberStatus chatMemberStatus) {
-        this.chatMemberStatus = chatMemberStatus;
-    }
-
-    public Member getMember() {
-        return member;
-    }
-
-    public ChatMemberStatus getChatMemberStatus() {
-        return chatMemberStatus;
-    }
-}
+    @Column(name = "status")
+    var chatMemberStatus: ChatMemberStatus = ChatMemberStatus.ONLINE
+) : BaseEntity()
--- NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/chat/domain/ChatRoom.kt
+++ NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/chat/domain/ChatRoom.kt
@@ -1,64 +1,22 @@
-package io.twogether.nbe_5_7_2_02team.chat.domain;
+package io.twogether.nbe_5_7_2_02team.chat.domain
 
-import io.twogether.nbe_5_7_2_02team.global.common.BaseEntity;
-import io.twogether.nbe_5_7_2_02team.post.domain.Post;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import io.twogether.nbe_5_7_2_02team.global.common.BaseEntity
+import io.twogether.nbe_5_7_2_02team.post.domain.Post
+import jakarta.persistence.*
 
 @Entity
-@Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ChatRoom extends BaseEntity {
-
+class ChatRoom(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    val id: Long? = null,
 
-    @OneToOne
     @JoinColumn(name = "post_id")
-    private Post post;
-
-    public void setMemberCount(Long memberCount) {
-        this.memberCount = memberCount;
-    }
-
-    public void setLastChatId(Long lastChatId) {
-        this.lastChatId = lastChatId;
-    }
-
+    @OneToOne
+    val post: Post,
+) : BaseEntity() {
     @Column(name = "member_count")
-    private Long memberCount = 0L;
+    var memberCount: Long = 0L
 
     @Column(name = "last_chat_id")
-    private Long lastChatId = 0L;
-
-    public ChatRoom(Post post) {
-        this.post = post;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Post getPost() {
-        return post;
-    }
-
-    public Long getMemberCount() {
-        return memberCount;
-    }
-
-    public Long getLastChatId() {
-        return lastChatId;
-    }
+    var lastChatId: Long = 0L
 }
