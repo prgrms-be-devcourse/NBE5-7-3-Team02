93e69439e986b3b2d4623ae91fd67629ef05071a
ksw733 <129919107+ksw733@users.noreply.github.com>
2025-06-18 02:04:18 +0000

    chore: Kotlin 스타일 수정

--- NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/oauth/domain/RefreshTokenBlackList.kt
+++ NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/oauth/domain/RefreshTokenBlackList.kt
@@ -2,9 +2,6 @@ package io.twogether.nbe_5_7_2_02team.oauth.domain
 
 import io.twogether.nbe_5_7_2_02team.global.common.BaseEntity
 import jakarta.persistence.*
-import lombok.AccessLevel
-import lombok.Getter
-import lombok.NoArgsConstructor
 
 @Entity
 class RefreshTokenBlackList(
