79d98092daffe9251ad2609a48225d20e5483998
STGRAM\gffd9 <29088981l@gmail.com>
2025-06-18 10:25:28 +0900

    build.gradle.kts Merge Error 수정

--- NBE_5_7_2_02TEAM/build.gradle.kts
+++ NBE_5_7_2_02TEAM/build.gradle.kts
@@ -77,13 +77,10 @@ dependencies {
     testImplementation("org.testcontainers:mysql")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("com.github.database-rider:rider-spring:1.44.0")
-<<<<<<< Updated upstream
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-=======
-
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.9.1")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:5.9.1")
->>>>>>> Stashed changes
+
 }
 
 tasks.test {
